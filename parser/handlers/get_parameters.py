ea47e25eb64df65761e3be795d89c136ba4cf7c5
oleg <oleg20064@gmail.com>
2023-03-04 13:19:39 +0100

    Изменил логику работы парсера

--- parser/handlers/get_parameters.py
+++ parser/handlers/get_parameters.py
@@ -1,47 +1,54 @@
-from urllib.parse import urlencode
+import re
+from urllib.parse import urlencode, unquote
 
 import requests
 
 
-async def check_subcategory(result, category_id):
-    match category_id:
-        case 4:
-            pass
-        case 1:
-            category_group = next(filter(lambda obj: obj['id'] == 'categoryGroup', result['result']['params']), None)
-            all_subcategories = next(filter(lambda obj: obj['id'] == 'categoryId', category_group['parameters']), None)
-            return all_subcategories['values']
+async def get_main_category():
+    with requests.get("https://www.avito.ru/web/1/category/tree") as response:
+        result: dict = response.json()
+    all_categories = [{"id": obj['categoryId'], "name": obj['name']} for obj in result['items'] if
+                      not obj.get('parentId')]
+    return all_categories
+
+
+async def get_subcategory(category_id, parent_category_id=None):
+    with requests.get("https://www.avito.ru/web/1/category/tree", timeout=5) as response:
+        result: dict = response.json()
+    if not parent_category_id:
+        main_category = next(filter(lambda obj: obj['categoryId'] == category_id, result['items']), None)
+        all_subcategories = [
+            {"id": obj['categoryId'], "name": obj['name'], "forParentId": obj['id'],
+             "param": list(map(lambda x: unquote(x),
+                               re.findall(r"params%5B\d+%5D=\d+", obj.get('deeplink', None)) if obj.get('deeplink',
+                                                                                                        None) else ''))}
+            for obj in result['items'] if obj.get('parentId', None) == main_category['id']]
+    else:
+        all_subcategories = [
+            {"id": obj['categoryId'], "name": obj['name'], "forParentId": obj['id'],
+             "param": list(map(lambda x: unquote(x),
+                               re.findall(r"params%5B\d+%5D=\d+", obj.get('deeplink', None)) if obj.get('deeplink',
+                                                                                                        None) else ''))}
+            for obj in result['items'] if obj.get('parentId', None) == parent_category_id]
+    return all_subcategories
 
 
 async def get_params(data, type_params, all_filters=None):
-    url = "https://m.avito.ru/api/6/search/parameters?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir"
-    query = [(k, v) for k, v in data.items() if v is not None]
-    params = urlencode(query)
-    with requests.get(f"{url}&{params}") as response:
-        result = response.json()
     match type_params:
         case "main_category":
-            category_group = next(filter(lambda obj: obj['id'] == 'categoryGroup', result['result']['params']), None)
-            all_categories = next(filter(lambda obj: obj['id'] == 'categoryId', category_group['parameters']), None)
-            return all_categories['values']
+            return await get_main_category()
         case "subcategory":
-            return await check_subcategory(result, data['categoryId'])
+            return await get_subcategory(data['categoryId'], data.get('parentId'))
         case "filters":
+            url = "https://m.avito.ru/api/6/search/parameters?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir"
+            params = urlencode([(k, v) for k, v in data.items() if v is not None])
+            with requests.get(f"{url}&{params}") as response:
+                result = response.json()
             all_select = list(
                 filter(lambda obj: obj['id'] != 'sort' and obj['type'] == 'select', result['result']['params']))
             all_multiselect = list(filter(lambda obj: obj['type'] == 'multiselect', result['result']['params']))
             all_groups = list(filter(
                 lambda obj: any(item in obj['id'] for item in ["param", "price", "brand"]) and obj['type'] == 'group',
                 result['result']['params']))
+            print(result)
             return all_select, all_multiselect, all_groups
-    print(result)
-
-
-tet = [
-    {"id": 1},
-    {"id": 2},
-    {"id": 3},
-    {"id": 4},
-    {"id": 5},
-    {"id": 6},
-]
--- parser/utils/database/pydantic_models.py
+++ parser/utils/database/pydantic_models.py
@@ -13,6 +13,7 @@ class DataAnnouncementAndParams(BaseModel):
     locationId: int = None
     geoCoords: Tuple[float, float] = None
     filters: bool = False
+    parentId: int = None
 
 
 # https://m.avito.ru/api/6/search/parameters?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&categoryId=20&locationId=637640
